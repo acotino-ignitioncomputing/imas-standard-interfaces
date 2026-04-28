bbb4aae92d1b14974bef174f4efa4a5d4199632b
Alan <acotino@ignitioncomputing.com>
2026-04-28 18:08:13 +0200

    Updated validation script based on JSON Schema

--- schemas/validate_definitions.py
+++ schemas/validate_definitions.py
@@ -1,24 +1,50 @@
-"""Script for validating interface definitions against LinkML model and
-IMAS Data Dictionary"""
+"""Script for validating interface definitions against JSON Schema and IMAS Data
+Dictionary"""
 
-from linkml.validator import validate
+import jsonschema
 from imas import IDSFactory, util
 import re
 
 
 def validate_against_schema(definition: dict, schema: dict) -> bool:
     """
-    Check correctness of the layout of definition with respect to LinkML schema
+    Check correctness of the layout of definition with respect to JSON Schema
     """
 
-    report = validate(definition, schema, "InterfaceDefinition")
+    validator = jsonschema.Draft202012Validator(schema)
 
-    if not report.results:
-        return True
-    else:
-        for result in report.results:
-            print(f"\t{result.message}")
-        return False
+    found_errors = validator.iter_errors(definition)
+
+    validation_correct = True
+    for error in sorted(found_errors, key=str):
+        validation_correct = False
+        print(f"\tError at JSON path {error.json_path}\n")
+        print(f"\t{error.message}\n")
+
+    return validation_correct
+
+
+def extract_paths(definition: dict) -> list:
+    # Collect all IDS paths
+    path_list = []
+
+    if "paths" in definition:
+        path_list += definition["paths"]
+
+    if "optional_paths" in definition:
+        for criterium_dict in definition["optional_paths"]:
+            # Extract paths from (sub)lists
+            for sublist in criterium_dict.values():
+                for entry in sublist:
+                    if type(entry) is str:
+                        path_list.append(entry)
+                    elif type(entry) is list:
+                        path_list += entry
+                    else:
+                        raise (
+                            Exception(f"Invalid entry '{entry}' in {criterium_dict}")
+                        )
+    return sorted(path_list)
 
 
 def check_ids_name(definition: dict) -> bool:
@@ -29,8 +55,12 @@ def check_ids_name(definition: dict) -> bool:
     ids_names_list = IDSFactory(dd_version).ids_names()
 
     correct_ids_names = True
-    for ids in definition["ids"]:
-        ids_name = ids["name"]
+
+    # Collect all IDS paths
+    path_list = extract_paths(definition)
+
+    for ids_path in path_list:
+        ids_name = ids_path.split("/")[0]
         if ids_name not in ids_names_list:
             print(
                 f"\tIDS name '{ids_name}' is not in Data Dictionary version {dd_version}"
@@ -45,40 +75,27 @@ def check_ids_paths_in_dd(definition: dict) -> bool:
     dd_version = definition["dd_version"]
 
     all_paths_valid = True
-    for ids_dict in definition["ids"]:
-        ids_name = ids_dict["name"]
 
-        # Create empty IDS to extract valid paths from
+    # Collect all IDS paths
+    path_list = extract_paths(definition)
+
+    for full_ids_path in path_list:
+        # Extract IDS name and path, and remove any index notation
+        ids_name = full_ids_path.split("/")[0]
+        ids_path = full_ids_path.replace(f"{ids_name}/", "")
+        ids_path = re.sub(r"\(.{1,6}\)", "", ids_path)
+
+        # Create empty IDS to extract valid paths
         ids_instance = IDSFactory(dd_version).new(ids_name)
 
         valid_paths_list = util.find_paths(ids_instance, "")
 
-        # First check paths in key 'required_paths'
-        if "required_paths" in ids_dict:
-            for ids_path in ids_dict["required_paths"]:
-                # Remove index-notation (if present)
-                ids_path_string = re.sub(r"\(.{1,6}\)", "", ids_path)
-
-                if ids_path_string not in valid_paths_list:
-                    print(
-                        f"\tIDS path {ids_path_string} is not in IDS {ids_name} for "
-                        + f"Data Dictionary version {dd_version}."
-                    )
-                    all_paths_valid = False
-
-        # Next, check paths in key 'constraints'
-        if "constraints" in ids_dict:
-            for constraint_dict in ids_dict["constraints"]:
-                for ids_path in constraint_dict["path"]:
-                    # Remove index-notation (if present)
-                    ids_path_string = re.sub(r"\(.{1,6}\)", "", ids_path)
-
-                    if ids_path_string not in valid_paths_list:
-                        print(
-                            f"\tIDS path {ids_path_string} is not in IDS {ids_name} for "
-                            + f"Data Dictionary version {dd_version}."
-                        )
-                        all_paths_valid = False
+        if ids_path not in valid_paths_list:
+            print(
+                f"\tIDS path {ids_path} is not in IDS {ids_name} for "
+                + f"Data Dictionary version {dd_version}."
+            )
+            all_paths_valid = False
 
     return all_paths_valid
 
@@ -88,30 +105,45 @@ def check_ids_paths_in_dd(definition: dict) -> bool:
 
 if __name__ == "__main__":
     from pathlib import Path
+    import json
     import yaml
 
-    schema_path = Path(__file__).parents[0] / "LinkML_schema.yaml"
+    schema_path = Path(__file__).parents[0] / "json_schema.json"
     with open(schema_path) as file:
-        schema_dict = yaml.safe_load(file)
+        schema_dict = json.load(file)
 
-    folder = Path(__file__).parents[1] / "definitions"
+    folder = Path(__file__).parents[1] / "example_definitions"
 
-    for file_path in sorted(folder.glob("**/*.yaml")):
+    incorrect_definitions = []
+
+    # for file_path in sorted(folder.glob("**/*.yaml")):
+    for file_path in [Path("nice_inv_input_v73.yaml")]:
         with open(file_path) as file:
             definition_dict = yaml.safe_load(file)
 
         print(f"\nValidating {file_path.name}...")
 
-        # Correctness with respect to LinkML
+        # Correctness with respect to JSON Schema
         if not validate_against_schema(definition_dict, schema_dict):
+            incorrect_definitions.append(file_path.name)
             continue
 
         #  Check IDS names are in Data Dictionary
-        if "ids" in definition_dict and not check_ids_name(definition_dict):
+        if not check_ids_name(definition_dict):
+            incorrect_definitions.append(file_path.name)
             continue
 
         # Check IDS paths are in Data Dictionary
-        if "ids" in definition_dict and not check_ids_paths_in_dd(definition_dict):
+        if not check_ids_paths_in_dd(definition_dict):
+            incorrect_definitions.append(file_path.name)
             continue
+        """ 
+        """
 
         print("\tAll checks passed")
+
+    if incorrect_definitions:
+        print("\nIssues were found in the following file(s)")
+        print("\n\t".join(incorrect_definitions))
+    else:
+        print("\nNo issues found")
