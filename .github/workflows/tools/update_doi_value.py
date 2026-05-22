6462526d61c6bb0fc0ce0bd5b47cfd8cbc15ed71
Alan <acotino@ignitioncomputing.com>
2026-05-22 15:51:29 +0200

    In update_doi_value.py, minor corrections for reduction of code lines

--- .github/workflows/tools/update_doi_value.py
+++ .github/workflows/tools/update_doi_value.py
@@ -16,16 +16,14 @@ def update_doi_value(new_doi: str):
     # TODO: Add proper error handling
 
     # First, update schema
-    with open(SCHEMA_PATH, "r") as schema_file:
-        schema_dict = json.load(schema_file)
+    schema_dict = json.loads(SCHEMA_PATH.read_text())
 
     schema_dict["properties"][SCHEMA_VERSION_KEY]["const"] = new_doi
 
-    with open(SCHEMA_PATH, "w") as schema_file:
-        schema_file = json.dump(schema_dict, schema_file, indent=4)
+    SCHEMA_PATH.write_text(json.dumps(schema_dict, indent=4))
 
     # Next, update each YAML file in folder example_definitions
-    for yaml_file_path in sorted(EXAMPLE_DEFINITIONS_FOLDER.glob("**/*.yaml")):
+    for yaml_file_path in EXAMPLE_DEFINITIONS_FOLDER.glob("**/*.yaml"):
         with open(yaml_file_path, "r") as yaml_file:
             # Only edit line with key 'schema_version' to preserve newlines & spacing
             lines = yaml_file.readlines()
