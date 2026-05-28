f0ca5c5277d811dc80cdb9e5e46723da9930a6c9
Alan <acotino@ignitioncomputing.com>
2026-05-28 15:24:12 +0200

    Adjusted test_publish_on_zenodo.py to test the scripts in folder .github/workflows/tools related to the workflow publish_schema.yaml.

--- .github/workflows/tools/tests/test_publish_on_zenodo.py
+++ .github/workflows/tools/tests/test_publish_on_zenodo.py
@@ -3,11 +3,14 @@ Test publishing using Zenodo's sandbox environment https://sandbox.zenodo.org
 
 Run using
     cd .github/workflow
-    uv run --group tests python -m tools.tests.test_publish_on_zenodo
+    uv run --group zenodo_api python -m tools.tests.test_publish_on_zenodo
 """
 
 import os
-from tools.publish_on_zenodo import main
+from tools.get_latest_draft_url import get_latest_draft_url
+from tools.construct_new_doi import construct_new_doi
+from tools.update_doi_value import update_doi_value
+from tools.publish_on_zenodo import publish_on_zenodo
 
 SANDBOX_ZENODO_URL = "https://sandbox.zenodo.org/api/deposit/depositions"  # Sandbox
 SANDBOX_ZENODO_PREFIX = "10.5072"  # Sandbox
@@ -20,12 +23,16 @@ ZENODO_URL, ZENODO_PREFIX, ZENODO_API_KEY = (
 )
 
 choice = input(
-    "WARNING: \n\tThis test runs the main function of 'publish_on_zenodo.py'.\n"
-    + "\tThis WILL push a new commit to the current branch using git.\n"
+    "WARNING: \n\tThis test runs the Python functions used in the publish_schema.yaml workflow.\n"
+    + "\tThis WILL update the DOI URL in the JSON Schema and example definitions.\n"
     + "\tIf this is expected, type 'yes':\n"
     + "Continue? "
 )
 if choice == "yes":
-    main()
+    print("""Testing the creation of a new version on Zenodo's sandbox""")
+    latest_draft_url = get_latest_draft_url()
+    new_doi_url = construct_new_doi(latest_draft_url)
+    update_doi_value(new_doi_url)
+    publish_on_zenodo(latest_draft_url)
 else:
     print("Test aborted")
