4f35046254e9ce438483cf12fdb54579b01dbfc8
Alan <acotino@ignitioncomputing.com>
2026-07-06 17:19:57 +0200

    In compatibility check, ensured log messages are displayed if issues are found. Also, moved sys.exit to main and let function check_compatibility return integer-exit-code

--- interfaceTools/check_compatibility.py
+++ interfaceTools/check_compatibility.py
@@ -6,7 +6,7 @@ import logging
 import sys
 import yaml
 
-from .validate_definitions import validate_definitions, extract_paths
+from validate_definitions import validate_definitions, extract_paths
 
 # Global parameters used for consistent amount of spacing, independent of user config
 SPACING_2 = "  "
@@ -110,16 +110,11 @@ def check_any_of_blocks(interface_A_dict, interface_B_dict):
 
 def check_allowed_values(interface_A_dict, interface_B_dict):
     # docstring
-    # return list[str]: paths which don't satisfy the allowed_value constraint in
-    # interface B
+    # return list[str]: paths which don't satisfy the allowed_value constraint in interface B
 
-    return
+    return []
 
 
-@click.command()
-@click.argument("path_interface_A")
-@click.argument("path_interface_B")
-@click.option("-s", "--silent", is_flag=True, help="If set, supress any log messages")
 def check_compatibility(path_interface_A: str, path_interface_B: str, silent: bool):
     """Check if the provided output interface is compatible with the provided
     input interface.
@@ -140,8 +135,17 @@ def check_compatibility(path_interface_A: str, path_interface_B: str, silent: bo
         logger.setLevel(logging.WARNING)
 
     # Ensure interfaces validate against schema
-    validate_definitions(path_interface_A, silent)
-    validate_definitions(path_interface_B, silent)
+    if validate_definitions(path_interface_A, silent=True) != 0:
+        logger.warning(
+            f"Interface '{path_interface_A}' does not validate against schema."
+        )
+        return 1
+
+    if validate_definitions(path_interface_B, silent=True) != 0:
+        logger.warning(
+            f"Interface '{path_interface_B}' does not validate against schema."
+        )
+        return 1
 
     # Load YAML files
     path_interface_A = Path(path_interface_A)
@@ -154,14 +158,14 @@ def check_compatibility(path_interface_A: str, path_interface_B: str, silent: bo
         interface_B_dict = yaml.safe_load(file)
 
     logger.warning(
-        f"\nCheck if interface {path_interface_A} is compatible with"
-        f" {path_interface_B}..."
+        f"\nCheck if interface '{path_interface_A}' is compatible with"
+        f" '{path_interface_B}' ..."
     )
 
     # If interface_A contains optional paths then 'compatibility' is ill-defined
     if any(
         [
-            isinstance[entry, dict] and ("any_of" in entry or "all_or_none" in entry)
+            isinstance(entry, dict) and ("any_of" in entry or "all_or_none" in entry)
             for entry in interface_A_dict
         ]
     ):
@@ -173,10 +177,11 @@ def check_compatibility(path_interface_A: str, path_interface_B: str, silent: bo
     # First check mandatory IDS paths, and collect which are missing
     missing_mandatory_paths = check_mandatory_paths(interface_A_dict, interface_B_dict)
 
-    logger.warning(
-        f"{SPACING_2}Following mandatory paths are missing:"
-        + f"\n{SPACING_4}".join(missing_mandatory_paths)
-    )
+    if missing_mandatory_paths:
+        logger.warning(
+            f"{SPACING_2}Following mandatory paths are missing:"
+            + f"\n{SPACING_4}".join(missing_mandatory_paths)
+        )
 
     # Next, check for the optional paths in interface_B.
 
@@ -185,22 +190,24 @@ def check_compatibility(path_interface_A: str, path_interface_B: str, silent: bo
     missing_all_or_none = check_all_or_none_block(interface_A_dict, interface_B_dict)
 
     # Logging based on missing_all_or_none
-    logger.warnin(
-        f"{SPACING_2}Following paths are under an all_or_none-key, but not all are "
-        + f"present or absent in interface {path_interface_A}:"
-        + f"\n{SPACING_4}".join(missing_all_or_none)
-    )
+    if missing_all_or_none:
+        logger.warning(
+            f"{SPACING_2}Following paths are under an all_or_none-key, but not all are "
+            + f"present or absent in interface {path_interface_A}:"
+            + f"\n{SPACING_4}".join(missing_all_or_none)
+        )
 
     # Check the paths under each any_of-block
     missing_any_of = check_any_of_blocks(interface_A_dict, interface_B_dict)
 
-    logger.warning(
-        f"{SPACING_2}The following paths are listed as a subset under an any_of-block,"
-        f" but no subset was contained in interface {path_interface_A}:"
-        + f"\n{SPACING_4}".join(missing_any_of)
-    )
+    if missing_any_of:
+        logger.warning(
+            f"{SPACING_2}The following paths are listed as a subset under an any_of-block,"
+            f" but no subset was contained in interface {path_interface_A}:"
+            + f"\n{SPACING_4}".join(missing_any_of)
+        )
 
-    # Lastly, check for allowed_values
+    # TODO: Lastly, check for allowed_values
     missing_allowed_values = check_allowed_values(interface_A_dict, interface_B_dict)
 
     if (
@@ -209,20 +216,21 @@ def check_compatibility(path_interface_A: str, path_interface_B: str, silent: bo
         and missing_any_of
         and missing_allowed_values
     ):
-        if not silent:
-            logger.warning("\nInterfaces are not compatible")
+        logger.warning("\nInterfaces are not compatible")
 
-        sys.exit(1)
+        return 1
     else:
-        logger.warning("\nNo issues found")
+        logger.warning("\nNo issues found in the compability check")
+        return 0
 
 
 @click.command()
-@click.argument("path_interface_A")
-@click.argument("path_interface_B")
+@click.argument("path_interface_a")
+@click.argument("path_interface_b")
 @click.option("-s", "--silent", is_flag=True, help="If set, supress any log messages")
-def main(path_interface_A: str, path_interface_B: str, silent: bool):
-    check_compatibility(path_interface_A, path_interface_B, silent)
+def main(path_interface_a: str, path_interface_b: str, silent: bool):
+    error_code = check_compatibility(path_interface_a, path_interface_b, silent)
+    sys.exit(error_code)
 
 
 if __name__ == "__main__":
