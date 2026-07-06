a81f693d7f3a4f0990233e1616a185681d5ceb0f
Alan <acotino@ignitioncomputing.com>
2026-07-06 17:53:46 +0200

    added compatibility_check to init-file and explicitly added interfaceTools as package in pyproject.toml

--- interfaceTools/__init__.py
+++ interfaceTools/__init__.py
@@ -1,3 +1,4 @@
 from .validate_definitions import validate_definitions
+from .check_compatibility import check_compatibility
 
-__all__ = ["validate_definitions"]
+__all__ = ["validate_definitions", "check_compatibility"]
