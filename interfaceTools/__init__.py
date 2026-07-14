dbc798a682e2e94d445202931908dfed5b620360
Alan <acotino@ignitioncomputing.com>
2026-07-14 17:09:31 +0200

    Added dataset_compliance to init file

--- interfaceTools/__init__.py
+++ interfaceTools/__init__.py
@@ -1,4 +1,5 @@
 from .validate_definitions import validate_definitions
 from .check_compatibility import check_compatibility
+from .dataset_compliance import dataset_compliance
 
-__all__ = ["validate_definitions", "check_compatibility"]
+__all__ = ["validate_definitions", "check_compatibility", "dataset_compliance"]
