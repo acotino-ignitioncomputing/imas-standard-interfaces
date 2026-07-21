66ea7ff1f62027fa57be0f8057e8472e3b65b948
Sebbe Blokhuizen <sblokhuizen@ignitioncomputing.com>
2026-07-21 11:51:07 +0200

    add ruff to CI

--- interfaceTools/validate_definitions.py
+++ interfaceTools/validate_definitions.py
@@ -1,13 +1,14 @@
 """Script for validating interface definitions against JSON Schema and IMAS Data
 Dictionary"""
 
-import jsonschema
-from imas import dd_zip, IDSFactory, util, setup_logging
-from pathlib import Path
-from packaging.version import Version
 import json
 import logging
+from pathlib import Path
+
+import jsonschema
 import yaml
+from imas import IDSFactory, dd_zip, setup_logging, util
+from packaging.version import Version
 
 logger = logging.getLogger("validateLogger")
 handler = logging.StreamHandler()
@@ -131,7 +132,8 @@ def extract_paths(paths: list[str | dict]) -> list[str]:
         else:
             raise (
                 Exception(
-                    f"Invalid entry \n{SPACING_2}'{string_or_dict}'\n in {string_or_dict}"
+                    f"Invalid entry \n{SPACING_2}'{string_or_dict}'\n in "
+                    f"{string_or_dict}"
                 )
             )
     return sorted(path_list)
@@ -182,7 +184,6 @@ def check_ids_name(definition: dict) -> bool:
 
     correct_ids_names = True
     for dd_version in dd_versions_to_check:
-
         ids_names_list = IDSFactory(dd_version).ids_names()
 
         # Collect all IDS paths
@@ -218,7 +219,6 @@ def check_ids_paths_in_dd(definition: dict) -> bool:
     all_paths_valid = True
 
     for dd_version in dd_versions_to_check:
-
         # Collect all IDS paths
         path_list = extract_paths(definition["paths"])
 
